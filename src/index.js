47bd8e4baf40e9e296a4ea89518082f4c135b8c5
Sylvia <sylviapap1@gmail.com>
2020-04-21 17:03:16 -0700

    removed service worker, dev tool thing? most basic thunk

--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import * as serviceWorker from './serviceWorker';
-
 import { BrowserRouter as Router} from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
 import './index.css';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middleware = [thunk]
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(...middleware)
-  ))
-
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <div>
@@ -31,6 +24,3 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
-
-
-serviceWorker.unregister();
