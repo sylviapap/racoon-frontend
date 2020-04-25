354de448636b9403dc07b29a194c4702285060eb
Sylvia <sylviapap1@gmail.com>
2020-04-24 20:45:58 -0700

    minimal css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,14 +15,14 @@ class Profile extends Component {
 
   render() {
     return(
-    <div className="profile">
+    <div className="profile row">
       {!!this.props.user.id ? 
         <h2 className="welcome">Welcome {this.props.user.username}</h2>
       :
         <p>You are not logged in</p>
       }
 
-      <div className="bookmarks">
+      <div className="bookmarks column">
         <h2>Your Bookmarks</h2>
         { this.props.bookmarks.length === 0 ? 
         <h4 className="no-markers">Looks like you have no bookmarks..</h4> 
@@ -30,7 +30,7 @@ class Profile extends Component {
         this.props.bookmarks.map(bookmark => <CreatedMarker key={bookmark.id} marker={bookmark}/>)
         }
       </div>
-      <div className="created-markers">
+      <div className="created-markers column">
         <h2>Your Created Markers</h2>
         { this.props.createdMarkers.length === 0 ?
         <h4 className="no-markers">Looks like you haven't posted a marker..</h4> 
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
-// import './index.css';
+import './index.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
