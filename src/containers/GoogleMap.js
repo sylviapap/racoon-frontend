9d5d7bce68342e6ba58828968ebb5537382c7ea6
Sylvia <sylviapap1@gmail.com>
2020-04-30 11:11:04 -0700

    messing with info window components, changed 'format' naming to 'ref'

--- src/containers/GoogleMap.js
+++ src/containers/GoogleMap.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-import InfoWindowFormat  from '../components/InfoWindowFormat'
+import { Map, GoogleApiWrapper, Marker, Circle, Polygon, Polyline } from 'google-maps-react';
+import InfoWindowRef  from '../components/InfoWindowRef'
 import cough from '../cough.png'
 import {styles} from '../services/mapStyles'
 
@@ -33,61 +33,103 @@ class GoogleMap extends Component {
         }
       };
 
-    handleMarkerViewInfoClick = (place) => {
+    handleMoreInfoClick = (place) => {
       let markerId = place.id
+      console.log(markerId)
       this.props.history.push(`/markers/${markerId}`)
     }
+
      
     render() {
-      console.log(this.props.initialMapData)
+      const coords = {lat: 0, lng: -98.5794797};
+      const population = 2714856;
+      // const cityCircle = new google.maps.Circle({
+      //   strokeColor: '#FF0000',
+      //   strokeOpacity: 0.8,
+      //   strokeWeight: 2,
+      //   fillColor: '#FF0000',
+      //   fillOpacity: 0.35,
+      //   map: this.map,
+      //   center: coords,
+      //   radius: Math.sqrt(population) * 100
+      // });
+      const triangleCoords = [
+        {lat: 25.774, lng: -80.190},
+        {lat: 18.466, lng: -66.118},
+        {lat: 32.321, lng: -64.757},
+        {lat: 25.774, lng: -80.190}
+      ];
       return (
         <div className="map">
         <Map 
-            google={this.props.google}
-            onClick={this.onMapClicked}
-            zoom={2}
-            minZoom={1.75}
-            styles={styles}
-            ref={(ref) => {this.map = ref}}
-            initialCenter={{lat: 0, lng: -98.5794797}}
-            bounds={this.state.bounds}>
+          google={this.props.google}
+          onClick={this.onMapClicked}
+          zoom={2}
+          minZoom={1.75}
+          styles={styles}
+          ref={(ref) => {this.map = ref}}
+          initialCenter={{lat: 0, lng: -98.5794797}}
+          bounds={this.state.bounds}
+         >
+          {/* <Polygon
+          paths={triangleCoords}
+          strokeColor="#0000FF"
+          strokeOpacity={0.8}
+          strokeWeight={2}
+          fillColor="#0000FF"
+          fillOpacity={0.35} /> */}
+
+          {/* <Polyline
+          path={triangleCoords}
+          strokeColor={"#0000FF"}
+          strokeOpacity={0.8}
+          strokeWeight={2} /> */}
+
+          <Circle
+          radius={1200000}
+          center={{lat: 0, lng: -98.5794797}}
+          strokeColor={"#FF0000"}
+          strokeOpacity={0}
+          strokeWeight={5}
+          fillColor={"#FF0000"}
+          fillOpacity={0.2}
+           />
                 
-                { this.props.initialMapData ? 
-                (this.props.initialMapData.map(object =>
-                    <Marker 
-                    onClick={this.onMarkerViewInfoWindowClick}
-                    position={{
-                      lat: object.latitude,
-                      lng: object.longitude}}
-                    title={object.title}
-                    address={object.address}
-                    comments={object.comments}
-                    id={object.id}
-                    key={object.id}
-                    symptoms={object.message}
-                    message="COVID-19 Self-Report"
-                    icon={cough}
-                    />))
-                    :
-                    null
-                }
+          { this.props.initialMapData ? 
+          (this.props.initialMapData.map(object =>
+              <Marker 
+              onClick={this.onMarkerViewInfoWindowClick}
+              position={{
+                lat: object.latitude,
+                lng: object.longitude}}
+              title={object.title}
+              address={object.address}
+              comments={object.comments}
+              id={object.id}
+              key={object.id}
+              symptoms={object.message}
+              message="COVID-19 Self-Report"
+              icon={cough}
+              />))
+              :
+              null
+          }
      
-            <InfoWindowFormat
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}>
-                <div className="info-window">
-                  <h3>{this.state.selectedPlace.message}</h3>
-                  <h3>Description: {this.state.selectedPlace.title}</h3>
-                  <p>Address: {this.state.selectedPlace.address}</p>
-                  <p>Symptoms: {this.state.selectedPlace.symptoms}</p>
-                  <button className="button" onClick={() => this.handleMarkerViewInfoClick(this.state.selectedPlace)} key={this.state.selectedPlace.id}>More Info</button>
-                  
-                </div>
-            </InfoWindowFormat>
-          </Map>
-          </div>
-        )
-    }
+          <InfoWindowRef
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+              <div className="info-window">
+              <h3>{this.state.selectedPlace.message}</h3>
+              <h3>{this.state.selectedPlace.title}</h3>
+              <p>Address: {this.state.selectedPlace.address}</p>
+              <p>Symptoms: {this.state.selectedPlace.symptoms}</p>
+              <button className="button" onClick={() => this.handleMoreInfoClick(this.state.selectedPlace)} key={this.state.selectedPlace.id}>More Info</button>
+              </div>
+          </InfoWindowRef>
+        </Map>
+      </div>
+    )
+  }
 }
 
 export default GoogleApiWrapper({
