c3f1dd93359d68fb7f171e10b7da72c7663df9c5
Sylvia <sylviapap1@gmail.com>
2020-05-03 17:28:59 -0700

    basic formatting, title case name

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,12 +8,15 @@ class Profile extends Component {
 
 
   render() {
+    let name = this.props.currentUser.first_name
+    let titleName = name.charAt(0).toUpperCase() + name.substr(1).toLowerCase()
+    console.log(titleName)
     return(
     <div className="profile page">
       <i className="fa fa-times-circle return" onClick={this.props.handleReturnClick}/>
-      <h1 className="welcome">My Health Profile</h1>
+      <h1 className="welcome">My Info</h1>
       {!!this.props.currentUser ? 
-        <p>Profile For: {this.props.currentUser.first_name} {this.props.currentUser.last_name}</p>
+        <p>Profile For: {titleName} {this.props.currentUser.last_name}</p>
       :
         <p>You are not logged in</p>
       }
@@ -21,15 +24,15 @@ class Profile extends Component {
       <div className="bookmarks card">
         <h2>Your Bookmarks</h2>
         { this.props.bookmarks.length === 0 ? 
-        <p className="no-markers">You have none.. go to a marker's info page to save a bookmark</p> 
+        <p className="no-markers">You have none...</p> 
         :
         this.props.bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} history={this.props.history}/>)
         }
       </div>
       <div className="created-markers card">
-        <h2>Markers You've Posted</h2>
+        <h2>Your Posts</h2>
         { this.props.createdMarkers.length === 0 ?
-        <p className="no-markers">You haven't posted yet... go to the map to post</p> 
+        <p className="no-markers">You haven't posted yet...</p> 
         :
         this.props.createdMarkers.map(marker => <CreatedMarker key={marker.id} marker={marker} history={this.props.history}/>) 
         }
