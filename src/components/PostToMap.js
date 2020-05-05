a0b1a91e68ccf204bbbc4795954cd722c4bbdd95
Sylvia <sylviapap1@gmail.com>
2020-05-05 11:34:51 -0700

    info span

--- src/components/PostToMap.js
+++ src/components/PostToMap.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import addToMap from '../actions/addToMap'
 import {API_ROOT} from '../services/api'
+import addSymptom from '../actions/addSymptom'
 
 class PostToMap extends Component{
   state = {
@@ -12,7 +13,7 @@ class PostToMap extends Component{
       address: []
     },
     symptomChoices: [],
-    symptoms: []
+    symptomsToAdd: []
   }
 
   componentDidMount() {
@@ -40,15 +41,16 @@ class PostToMap extends Component{
 
   handleSubmit = (event) => {
     event.preventDefault()
+    let userId = this.props.currentUser.id
     let markerData = {
-      user_id: this.props.currentUser.id,
+      user_id: userId,
       latitude: this.state.fields.latitude,
       longitude: this.state.fields.longitude,
       title: this.state.fields.title, 
-      address: this.state.fields.address,
-      message: this.state.symptoms.toString()
+      address: this.state.fields.address    
     }
     this.props.addToMap(event, markerData, this.props.history);
+    this.state.symptomsToAdd.map(s => this.props.addSymptom(event, userId, s, this.props.history))
     this.setState({ 
       fields: {
         latitude: [],
@@ -73,10 +75,11 @@ class PostToMap extends Component{
     return(
       <div className="post-container page">
         <i className="fa fa-times-circle return" onClick={this.props.handleReturnClick}/>
+        <span className="info">Please allow the browser to access your location and wait for coordinates to appear. You may select symptoms, which will then be saved to your profile, and all saved symptoms from your profile will be posted to your marker. You can add/remove symptoms separately in your profile, and changes will reflect in the updated marker.</span>
       <form onSubmit ={this.handleSubmit}className="post-form">
-        <label>Title</label>
+        <label>Caption (optional)</label>
         <input name="title" type="text" value={this.state.fields.title} onChange={this.handleChange}/>
-        <label>Address</label>
+        <label>Address (optional)</label>
         <input name="address" type="text" value={this.state.fields.address} onChange={this.handleChange}/>
         <label>Latitude</label>
         <input name="latitude" type="number" value={this.state.fields.latitude} onChange={this.handleChange}/>
@@ -86,12 +89,12 @@ class PostToMap extends Component{
         <label>Select your symptoms: (Hold Ctrl or Cmd to select multiple)</label>
             <select 
               multiple={true} 
-              value={this.state.symptoms} 
-              name="symptoms"
+              value={this.state.symptomsToAdd} 
+              name="symptomsToAdd"
               onChange={this.handleSelect} >
                 {this.state.symptomChoices.map(symptom => 
                   <option 
-                    value={symptom.common_name} 
+                    value={symptom.id} 
                     key={symptom.id}>
                       {symptom.common_name}
                   </option>)}
@@ -112,6 +115,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return{
+    addSymptom: (event, userId, symptomId, history) => {dispatch(addSymptom(event, userId, symptomId, history))},
     addToMap: (event, markerData, history) => {dispatch(addToMap(event, markerData, history))}
   }
 }
